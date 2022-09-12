2b00fe9dc6c9e2f4302134b0481d61881298ef11
Patryk Makowski <praca.makowski@gmail.com>
2022-09-12 16:30:48 +0200

    Fix countDown rendering

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,8 +1,9 @@
 import dayjs from "dayjs";
 import { createSignal, onCleanup } from "solid-js";
 
-const endDate = dayjs("October 20, 2022 00:00:00");
-const getDate = (date: dayjs.Dayjs) => {
+const endDate = dayjs("October 20, 2022 13:00:00");
+const getDate = () => {
+  const date = dayjs();
   const days = endDate.diff(date, "days");
   const hours = endDate.subtract(days, "days").diff(date, "hours");
   const minutes = endDate
@@ -13,15 +14,16 @@ const getDate = (date: dayjs.Dayjs) => {
 };
 
 const CountDown = () => {
-  const [currentDate, setCurrentDate] = createSignal(getDate(dayjs()));
+  const [currentDate, setCurrentDate] = createSignal(getDate());
 
-  const interval = setInterval(() => setCurrentDate(getDate(dayjs())), 60000);
+  const interval = setInterval(() => setCurrentDate(getDate()), 60000);
   onCleanup(() => clearInterval(interval));
 
   return (
     <h2 class="countdown">
-      {currentDate().days} DNI {currentDate().hours} GODZIN{" "}
-      {currentDate().minutes} MINUT
+      {`${currentDate().days} DNI ${currentDate().hours} GODZIN ${
+        currentDate().minutes
+      } MINUT`}
     </h2>
   );
 };
