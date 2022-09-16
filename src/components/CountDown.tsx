510263ba18aaa6830c8eb85beefb73dfd7a8c26b
Jacek Nowak <jaceknowak226@gmail.com>
2022-09-16 22:18:45 +0200

    Prettier config (#1)

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,15 +1,12 @@
-import dayjs from "dayjs";
-import { createSignal, onCleanup } from "solid-js";
+import dayjs from 'dayjs';
+import { createSignal, onCleanup } from 'solid-js';
 
-const endDate = dayjs("October 20, 2022 13:00:00");
+const endDate = dayjs('October 20, 2022 13:00:00');
 const getDate = () => {
   const date = dayjs();
-  const days = endDate.diff(date, "days");
-  const hours = endDate.subtract(days, "days").diff(date, "hours");
-  const minutes = endDate
-    .subtract(days, "days")
-    .subtract(hours, "hours")
-    .diff(date, "minutes");
+  const days = endDate.diff(date, 'days');
+  const hours = endDate.subtract(days, 'days').diff(date, 'hours');
+  const minutes = endDate.subtract(days, 'days').subtract(hours, 'hours').diff(date, 'minutes');
   return { days, hours, minutes };
 };
 
@@ -21,9 +18,7 @@ const CountDown = () => {
 
   return (
     <h2 class="countdown">
-      {`${currentDate().days} DNI ${currentDate().hours} GODZIN ${
-        currentDate().minutes
-      } MINUT`}
+      {`${currentDate().days} DNI ${currentDate().hours} GODZIN ${currentDate().minutes} MINUT`}
     </h2>
   );
 };
